908962747459110f943a00aa9de9b09279a3125e
mmdohmen <mmdohmen@hotmail.com>
2024-05-22 19:19:01 -0300

    issue 17 - relacion Pedido Cliente

--- src/main/java/com/progra3/proyecto/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.progra3.proyecto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.progra3.proyecto.entity.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
-}
