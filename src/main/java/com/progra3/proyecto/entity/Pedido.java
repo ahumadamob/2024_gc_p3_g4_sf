4c5511fe5bded0545697a0558384b2ae782902d5
IgnaaRo <ignaromero2003@gmail.com>
2024-05-21 23:47:04 -0300

    Relación entre pedido y producto

--- src/main/java/com/progra3/proyecto/entity/Pedido.java
+++ src/main/java/com/progra3/proyecto/entity/Pedido.java
@@ -1,83 +1,86 @@
 package com.progra3.proyecto.entity;
+
+import java.util.Set;
+
 import jakarta.persistence.*;
+
 @Entity
-@Table
 public class Pedido {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column
-	private int idPedido;
-	@Column
-	private String productos;
-	@Column
-	private String estado;
-	@Column
-	private String direccionEntrega;
-	@Column
-	private String metodoPago;
-	@Column
-	private String cliente;
-	@Column
-	private String restaurante;
-	
-	//Constructors
-	public Pedido() {
-	}
-	
-	public Pedido(int idPedido, String productos, String estado, String direccionEntrega, String metodoPago,
-			String cliente, String restaurante) {
-		this.idPedido = idPedido;
-		this.productos = productos;
-		this.estado = estado;
-		this.direccionEntrega = direccionEntrega;
-		this.metodoPago = metodoPago;
-		this.cliente = cliente;
-		this.restaurante = restaurante;
-	}
-	
-	//Getter and Setter
-	public int getIdPedido() {
-		return idPedido;
-	}
-	public void setIdPedido(int idPedido) {
-		this.idPedido = idPedido;
-	}
-	public String getProductos() {
-		return productos;
-	}
-	public void setProductos(String productos) {
-		this.productos = productos;
-	}
-	public String getEstado() {
-		return estado;
-	}
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-	public String getDireccionEntrega() {
-		return direccionEntrega;
-	}
-	public void setDireccionEntrega(String direccionEntrega) {
-		this.direccionEntrega = direccionEntrega;
-	}
-	public String getMetodoPago() {
-		return metodoPago;
-	}
-	public void setMetodoPago(String metodoPago) {
-		this.metodoPago = metodoPago;
-	}
-	public String getCliente() {
-		return cliente;
-	}
-	public void setCliente(String cliente) {
-		this.cliente = cliente;
-	}
-	public String getRestaurante() {
-		return restaurante;
-	}
-	public void setRestaurante(String restaurante) {
-		this.restaurante = restaurante;
-	}
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idPedido;
+
+    private String productos;
+    private String estado;
+    private String direccionEntrega;
+    private String metodoPago;
+    private String cliente;
+    private String restaurante;
+
+    @OneToMany(mappedBy = "pedido")
+    private Set<PedidoProducto> pedidoProductos;
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public String getProductos() {
+        return productos;
+    }
+
+    public void setProductos(String productos) {
+        this.productos = productos;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getDireccionEntrega() {
+        return direccionEntrega;
+    }
+
+    public void setDireccionEntrega(String direccionEntrega) {
+        this.direccionEntrega = direccionEntrega;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(String restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    public Set<PedidoProducto> getPedidoProductos() {
+        return pedidoProductos;
+    }
+
+    public void setPedidoProductos(Set<PedidoProducto> pedidoProductos) {
+        this.pedidoProductos = pedidoProductos;
+    }
 }
---
+++ src/main/java/com/progra3/proyecto/entity/PedidoProducto.java
@@ -0,0 +1,48 @@
+package com.progra3.proyecto.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class PedidoProducto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
