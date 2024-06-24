8e5a631a4fea6a48da24a746be369a5085a187c2
Nacho200031 <nachopujado@gmail.com>
2024-06-24 19:05:29 -0300

    Persistencia entidad/producto

---
+++ src/main/java/com/progra3/proyecto/service/IProductoService.java
@@ -0,0 +1,12 @@
+package com.progra3.proyecto.service;
+
+import java.util.List;
+import com.progra3.proyecto.entity.Producto;
+
+public interface IProductoService {
+    List<Producto> getAll();
+    Producto getById(Long id);
+    Producto save(Producto producto);
+    void delete(Long id);
+    boolean exists(Long id);
+  }
