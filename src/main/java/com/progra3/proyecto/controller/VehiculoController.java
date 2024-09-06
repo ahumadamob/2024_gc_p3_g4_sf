6c2f6f19789bf9b914bd900df55ff3ed0523e436
mmdohmen <mmdohmen@hotmail.com>
2024-09-06 14:11:37 -0300

    persistencia Vehiculo completa

--- src/main/java/com/progra3/proyecto/controller/VehiculoController.java
+++ src/main/java/com/progra3/proyecto/controller/VehiculoController.java
@@ -4,18 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.progra3.proyecto.entity.Repartidor;
 import com.progra3.proyecto.entity.Vehiculo;
 import com.progra3.proyecto.service.IVehiculoService;
+import com.progra3.proyecto.util.APIResponse;
+import com.progra3.proyecto.util.ResponseUtil;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/vehiculo")
+@RequestMapping("/api/v1/project/vehiculo")
 public class VehiculoController {
 
     @Autowired
     private IVehiculoService vehiculoService;
 
+    /*
     @GetMapping
     public ResponseEntity<List<Vehiculo>> getAllVehiculos() {
         List<Vehiculo> vehiculos = vehiculoService.getAll();
@@ -24,7 +28,20 @@ public class VehiculoController {
         }
         return ResponseEntity.ok(vehiculos);
     }
+    */
+    
+    @GetMapping
+	public ResponseEntity<APIResponse<List<Vehiculo>>> getAllVehiculos() {
+		List<Vehiculo> vehiculos = vehiculoService.getAll();
+		return vehiculos.isEmpty() ? ResponseUtil.notFound("NO hay VEHICULOS en la bbdd ...") : 
+			                         ResponseUtil.success(vehiculos);
+	}
+    
+    
+    // ======================================================================================================================
+    
 
+    /*
     @GetMapping("/{id}")
     public ResponseEntity<Vehiculo> getVehiculoById(@PathVariable("id") Integer id) {
         Vehiculo vehiculo = vehiculoService.getById(id);
@@ -33,7 +50,19 @@ public class VehiculoController {
         }
         return ResponseEntity.ok(vehiculo);
     }
+    */
+    
+    @GetMapping ("{id}")
+	public ResponseEntity<APIResponse<Vehiculo>> getVehiculoById(@PathVariable("id") Integer id) {
+		return vehiculoService.exists(id) ? ResponseUtil.success(vehiculoService.getById(id)) :
+			                                ResponseUtil.notFound("NO hay un VEHICULO con ese ID ...");
+	}
+    
+    
+    // ======================================================================================================================
+    
 
+    /*
     @PostMapping
     public ResponseEntity<Vehiculo> createVehiculo(@RequestBody Vehiculo vehiculo) {
         if (vehiculoService.exists(vehiculo.getId())) {
@@ -42,7 +71,20 @@ public class VehiculoController {
         Vehiculo savedVehiculo = vehiculoService.save(vehiculo);
         return ResponseEntity.ok(savedVehiculo);
     }
-
+    */
+    
+    @PostMapping
+	public ResponseEntity<APIResponse<Vehiculo>> createVehiculo(@RequestBody Vehiculo vehiculo) {
+		System.out.println(vehiculo.getId() + " - " + vehiculo.getNombre() + " - " + vehiculo.getTipo());
+		return vehiculoService.exists(vehiculo.getId()) ? ResponseUtil.badRequest("ya EXISTE un VEHICULO con ese ID") :
+			                                              ResponseUtil.success(vehiculoService.save(vehiculo));
+	}
+    
+    
+    // ======================================================================================================================
+    
+    
+    /*
     @PutMapping("/{id}")
     public ResponseEntity<Vehiculo> updateVehiculo(@PathVariable("id") Integer id, @RequestBody Vehiculo vehiculo) {
         if (!vehiculoService.exists(id)) {
@@ -52,7 +94,20 @@ public class VehiculoController {
         Vehiculo updatedVehiculo = vehiculoService.save(vehiculo);
         return ResponseEntity.ok(updatedVehiculo);
     }
-
+    */
+    
+    
+    @PutMapping
+	public ResponseEntity<APIResponse<Vehiculo>> updateVehiculo(@RequestBody Vehiculo vehiculo) {
+		return vehiculoService.exists(vehiculo.getId()) ? ResponseUtil.success(vehiculoService.save(vehiculo)) :
+			                                        ResponseUtil.badRequest("NO hay ningun VEHICULO con ese ID");
+	}
+    
+    
+    // ======================================================================================================================
+    
+    
+    /*
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteVehiculo(@PathVariable("id") Integer id) {
         if (!vehiculoService.exists(id)) {
@@ -61,4 +116,19 @@ public class VehiculoController {
         vehiculoService.delete(id);
         return ResponseEntity.noContent().build();
     }
+    */
+    
+    
+    @DeleteMapping ("{id}")
+	public ResponseEntity<APIResponse<Vehiculo>> deleteVehiculo (@PathVariable ("id") Integer id) {
+		if (vehiculoService.exists(id)) {
+			vehiculoService.delete(id);
+			return ResponseUtil.successDeleted("vehiculo ELIMINADO");
+		} else {
+			return ResponseUtil.badRequest("NO hay ningun VEHICULO con ese ID");
+		}
+	}
+    
+    
+    
 }
